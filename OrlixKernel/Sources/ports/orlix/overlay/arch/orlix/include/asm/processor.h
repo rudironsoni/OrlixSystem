2115f698ebb207089f8d80691965d28f15389c27
Rudimar Ronsoni <rudimar.ronsoni@outlook.com>
2026-05-22 16:46:19 +0200

    boot(kernel): add initial task switch context

--- OrlixKernel/Sources/ports/orlix/overlay/arch/orlix/include/asm/processor.h
+++ OrlixKernel/Sources/ports/orlix/overlay/arch/orlix/include/asm/processor.h
@@ -21,13 +21,29 @@ struct task_struct;
 void start_thread(struct pt_regs *regs, unsigned long pc, unsigned long sp);
 unsigned long __get_wchan(struct task_struct *p);
 
+struct orlix_cpu_context {
+	unsigned long x19;
+	unsigned long x20;
+	unsigned long x21;
+	unsigned long x22;
+	unsigned long x23;
+	unsigned long x24;
+	unsigned long x25;
+	unsigned long x26;
+	unsigned long x27;
+	unsigned long x28;
+	unsigned long fp;
+	unsigned long sp;
+	unsigned long pc;
+};
+
 struct thread_struct {
-	unsigned long ksp;
+	struct orlix_cpu_context cpu_context;
 };
 
 #define INIT_THREAD			\
 {					\
-	.ksp = 0,			\
+	.cpu_context = { 0 },		\
 }
 
 #define task_pt_regs(task)	\
--- OrlixKernel/Sources/ports/orlix/overlay/arch/orlix/kernel/process.c
+++ OrlixKernel/Sources/ports/orlix/overlay/arch/orlix/kernel/process.c
@@ -2,6 +2,7 @@
 
 #include <linux/sched.h>
 #include <linux/sched/task.h>
+#include <linux/sched/task_stack.h>
 #include <linux/string.h>
 #include <linux/errno.h>
 #include <linux/kernel.h>
@@ -9,6 +10,46 @@
 #include <asm/ptrace.h>
 
 struct thread_info *orlix_current_thread_info = &init_thread_info;
+
+asmlinkage void ret_from_fork(void);
+extern struct task_struct *orlix_cpu_switch_context(struct orlix_cpu_context *prev,
+						    struct orlix_cpu_context *next,
+						    struct task_struct *last);
+
+asm(
+".p2align 2\n"
+"	.globl _orlix_cpu_switch_context\n"
+"_orlix_cpu_switch_context:\n"
+"	mov	x9, sp\n"
+"	stp	x19, x20, [x0], #16\n"
+"	stp	x21, x22, [x0], #16\n"
+"	stp	x23, x24, [x0], #16\n"
+"	stp	x25, x26, [x0], #16\n"
+"	stp	x27, x28, [x0], #16\n"
+"	stp	x29, x9, [x0], #16\n"
+"	str	lr, [x0]\n"
+"	ldp	x19, x20, [x1], #16\n"
+"	ldp	x21, x22, [x1], #16\n"
+"	ldp	x23, x24, [x1], #16\n"
+"	ldp	x25, x26, [x1], #16\n"
+"	ldp	x27, x28, [x1], #16\n"
+"	ldp	x29, x9, [x1], #16\n"
+"	ldr	lr, [x1]\n"
+"	mov	sp, x9\n"
+"	mov	x0, x2\n"
+"	ret\n"
+".p2align 2\n"
+"	.globl _ret_from_fork\n"
+"_ret_from_fork:\n"
+"	bl	_schedule_tail\n"
+"	cbz	x19, 1f\n"
+"	mov	x0, x20\n"
+"	blr	x19\n"
+"	mov	x0, #0\n"
+"	bl	_do_exit\n"
+"1:\n"
+"	brk	#0\n"
+);
 
 void start_thread(struct pt_regs *regs, unsigned long pc, unsigned long sp)
 {
@@ -26,16 +67,30 @@ void flush_thread(void)
 
 int copy_thread(struct task_struct *p, const struct kernel_clone_args *args)
 {
-	(void)p;
-	(void)args;
-	return -EOPNOTSUPP;
+	struct pt_regs *childregs = task_pt_regs(p);
+
+	memset(&p->thread.cpu_context, 0, sizeof(p->thread.cpu_context));
+
+	if (!args->fn)
+		return -EOPNOTSUPP;
+
+	memset(childregs, 0, sizeof(*childregs));
+	childregs->pstate = PSR_MODE_EL1h;
+	childregs->syscallno = NO_SYSCALL;
+
+	p->thread.cpu_context.x19 = (unsigned long)args->fn;
+	p->thread.cpu_context.x20 = (unsigned long)args->fn_arg;
+	p->thread.cpu_context.sp = (unsigned long)childregs;
+	p->thread.cpu_context.pc = (unsigned long)ret_from_fork;
+
+	return 0;
 }
 
 struct task_struct *__switch_to(struct task_struct *prev, struct task_struct *next)
 {
-	(void)next;
-	panic("Orlix context switching is not implemented\n");
-	return prev;
+	orlix_current_thread_info = task_thread_info(next);
+	return orlix_cpu_switch_context(&prev->thread.cpu_context,
+					&next->thread.cpu_context, prev);
 }
 
 unsigned long __get_wchan(struct task_struct *p)
