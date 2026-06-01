2763260ff603f4b26618d5095cd880e6d1f32cb5
Rudimar Ronsoni <rudironsoni@RRJ-MacBook-Air-M1.users.madrid.bagatinironsoni.com>
2026-06-02 01:18:07 +0200

    Advance coreutils upstream test runtime

--- OrlixOS/Tests/coreutils_test_init.c
+++ OrlixOS/Tests/coreutils_test_init.c
@@ -41,7 +41,7 @@ static char *const test_envp[] = {
 	"PERL5LIB=/usr/lib/perl5/5.40.2",
 	"built_programs=[ b2sum base32 base64 basenc basename cat chgrp chmod chown chroot cksum comm cp csplit cut date dd df dir dircolors dirname du echo env expand expr factor false fmt fold groups head hostid id install join link ln logname ls md5sum mkdir mkfifo mknod mktemp mv nice nl nohup nproc numfmt od paste pathchk pinky pr printenv printf ptx pwd readlink realpath rm rmdir seq sha1sum sha224sum sha256sum sha384sum sha512sum shred shuf sleep sort split stat stty sum sync tac tail tee test timeout touch tr true truncate tsort tty uname unexpand uniq unlink users vdir wc who whoami yes ]",
 	"AWK=awk",
-	"COREUTILS_GROUPS=0 1",
+	"COREUTILS_GROUPS=0 2",
 	"EGREP=grep -E",
 	"EXEEXT=",
 	"MAKE=make",
@@ -100,7 +100,7 @@ static unsigned int count_tests(char *data, size_t size)
 
 static void ensure_runtime_filesystems(void)
 {
-	gid_t test_groups[] = {0, 1};
+	gid_t test_groups[] = {0, 2};
 
 	(void)mkdir("/proc", 0555);
 	(void)mkdir("/tmp", 01777);
